cab2e9f4d58c03bc66e087d5b50b5a63302e253e
Mathias Stav <mathias@feed.no>
2014-11-17 11:26:47 +0100

    all teh basics

---
+++ app/index.js
@@ -0,0 +1,85 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+
+var CraftcmsGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the marvelous Craftcms generator!'));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'Craft License',
+      message: 'Do you agree with the craft license?',
+      default: false
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+    this.copy('_gulpfile.js', 'gulpfile.js');
+    this.copy('_config.rb', 'config.rb');
+
+    this.mkdir('public');
+    this.copy('public/_index.php', 'public/index.php');
+    this.copy('public/_robots.txt', 'public/robots.txt');
+    this.copy('public/_web.config', 'public/web.config');
+    this.copy('public/htaccess', 'public/.htaccess');
+
+    this.mkdir('app');
+    this.mkdir('app/templates');
+    this.mkdir('app/templates/news');
+    this.mkdir('app/resources');
+    this.mkdir('app/resources/css');
+    this.mkdir('app/resources/js');
+    this.mkdir('app/resources/sass');
+    this.mkdir('app/resources/fonts');
+    this.mkdir('app/resources/images');
+
+    this.copy('app/_index.php', 'app/index.php');
+    this.copy('app/htaccess', 'app/.htaccess');
+    //Basic tamplates
+    this.copy('app/templates/_404.html', 'app/templates/404.html');
+    this.copy('app/templates/_layout.html', 'app/templates/_layout.html');
+    this.copy('app/templates/_index.html', 'app/templates/index.html');
+    // The basic news page templates
+    this.copy('app/templates/news/_entry.html', 'app/templates/news/_entry.html');
+    this.copy('app/templates/news/_index.html', 'app/templates/news/index.html');
+
+    //Resources
+    this.copy('app/resources/js/_app.js', 'app/resources/js/app.js');
+    this.copy('app/resources/sass/_main.scss', 'app/resources/sass/main.scss');
+
+  },
+
+  projectfiles: function () {
+    this.copy('bowerrc', '.bowerrc');
+    this.copy('gitignore', '.gitignore');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = CraftcmsGenerator;
---
+++ app/templates/_gulpfile.js
@@ -0,0 +1,130 @@
+var	gulp = require('gulp'),
+	compass = require('gulp-compass'),
+	watch = require('gulp-watch'),
+	livereload = require('gulp-livereload'),
+	prefix = require('gulp-autoprefixer'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	gutil = require('gulp-util'),
+	usemin = require('gulp-usemin'),
+	del = require('del'),
+	jshint = require('gulp-jshint'),
+	cache = require('gulp-cache'),
+	imagemin = require('gulp-imagemin'),
+	wiredep = require('wiredep').stream;
+
+
+//Watch
+
+gulp.task('server', function(next) {
+	var connect = require('connect'),
+			server = connect();
+	server.use(connect.static('app')).listen(process.env.PORT || 3000, next);
+});
+
+gulp.task('watch', ['server'], function() {
+
+	var server = livereload();
+
+	gulp.watch('app/resources/css/**').on('change', function(file) {
+		server.changed(file.path);
+	});
+
+	gulp.watch('app/resources/sass/**').on('change', function(file){
+		gulp.run('compass');
+	});
+
+	gulp.watch('app/resources/js/**').on('change', function(file){
+		gulp.run('jshint');
+	});
+
+	gulp.watch('app/resources/bower_components/**').on('change', function(file){
+		gulp.run('bower');
+	});
+
+});
+
+gulp.task('compass', function(){
+	gulp.src( ['./app/resources/sass/*.scss'], {base: './app/resources/sass/'} )
+		.pipe(compass({
+				config_file: './config.rb',
+				css: './app/resources/css',
+				sass: './app/resources/sass',
+		})).on('error', function(err) {
+				//console.log(err);
+		})
+		.pipe( prefix("last 3 version") )
+		.pipe( gulp.dest('./app/resources/css')
+	);
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('./app/resources/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('images', ['move'], function () {
+  return gulp.src('./app/resources/images/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('./public/resources/images'));
+
+});
+
+gulp.task('fonts', ['move'], function () {
+  return gulp.src('./app/resources/fonts/**/*')
+    .pipe(gulp.dest('./public/resources/fonts'));
+
+});
+
+//Build
+
+gulp.task('usemin', function() {
+
+	return gulp.src('app/templates/**')
+	.pipe(usemin({
+		js: [uglify()],
+		bower: [uglify()]
+	}))
+	.pipe(gulp.dest('./craft/templates'));
+
+});
+
+
+gulp.task('clean:before', function(cb) {
+    del(['public/resources/**'], cb);
+});
+
+gulp.task('clean:after', ['usemin', 'images', 'fonts'], function(cb) {
+    del(['craft/templates/resources/**', 'craft/templates/resources/'], cb);
+});
+
+gulp.task('move', ['usemin', 'clean:before'], function(){
+	//Move
+	return gulp.src(['craft/templates/resources/**'])
+	.pipe(gulp.dest('public/resources/'));
+});
+
+gulp.task('build', ['clean:after'], function(){
+
+
+});
+
+gulp.task('bower', function () {
+  gulp.src('./app/templates/_layout.html')
+    .pipe(wiredep({
+
+    }))
+    .pipe(gulp.dest('./app/templates/'));
+});
+
+gulp.task('default', ['build']);
+
+
+
+
+
+

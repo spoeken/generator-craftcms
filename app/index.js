828e63d18ae9b1ba7b626fbe58218565854537f7
Mathias Stav <mathias@feed.no>
2016-05-25 13:37:51 +0200

    major update

--- app/index.js
+++ app/index.js
@@ -60,44 +60,44 @@ var CraftcmsGenerator = yeoman.generators.Base.extend({
   },
 
   app: function () {
-    this.copy('_package.json', 'package.json');
+    //Config files
+    this.copy('_babelrc', '.babelrc');
+    this.copy('_bowerrc', '.bowerrc');
+    this.copy('_editorconfig', '.editorconfig');
+    this.copy('_gitignore', '.gitignore');
     this.copy('_bower.json', 'bower.json');
     this.copy('_gulpfile.js', 'gulpfile.js');
-    this.copy('_config.rb', 'config.rb');
+    this.copy('_package.json', 'package.json');
 
+
+    //Make folders
     this.mkdir('app');
     this.mkdir('app/templates');
     this.mkdir('app/templates/news');
     this.mkdir('app/resources');
-    this.mkdir('app/resources/css');
-    this.mkdir('app/resources/js');
-    this.mkdir('app/resources/sass');
+    this.mkdir('app/resources/scripts');
+    this.mkdir('app/resources/styles');
     this.mkdir('app/resources/fonts');
     this.mkdir('app/resources/images');
     this.mkdir('app/resources/json');
 
+    //Copy files
     this.copy('app/_index.php', 'app/index.php');
     this.copy('app/htaccess', 'app/.htaccess');
-    //Basic tamplates
+
+    // Templates
     this.copy('app/templates/_404.html', 'app/templates/404.html');
     this.copy('app/templates/_layout.html', 'app/templates/_layout.html');
     this.copy('app/templates/_index.html', 'app/templates/index.html');
-    // The basic news page templates
     this.copy('app/templates/news/_entry.html', 'app/templates/news/_entry.html');
     this.copy('app/templates/news/_index.html', 'app/templates/news/index.html');
 
     //Resources
-    this.copy('app/resources/js/_app.js', 'app/resources/js/app.js');
-    this.copy('app/resources/sass/_main.scss', 'app/resources/sass/main.scss');
-    this.copy('app/resources/css/_main.css', 'app/resources/css/main.css');
+    this.copy('app/resources/scripts/_main.js', 'app/resources/scripts/main.js');
+    this.copy('app/resources/scripts/_hello.js', 'app/resources/scripts/hello.js');
+    this.copy('app/resources/styles/_main.scss', 'app/resources/styles/main.scss');
 
     // this.installDependencies();
-  },
-
-  projectfiles: function () {
-    this.copy('bowerrc', '.bowerrc');
-    this.copy('gitignore', '.gitignore');
-    this.copy('jshintrc', '.jshintrc');
   }
 });
 
--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -1,141 +1,224 @@
-var	gulp = require('gulp'),
-	compass = require('gulp-compass'),
-	watch = require('gulp-watch'),
-	livereload = require('gulp-livereload'),
-	prefix = require('gulp-autoprefixer'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	gutil = require('gulp-util'),
-	usemin = require('gulp-usemin'),
-	del = require('del'),
-	jshint = require('gulp-jshint'),
-	cache = require('gulp-cache'),
-	imagemin = require('gulp-imagemin'),
-	wiredep = require('wiredep').stream;
+// generated on 2016-05-23 using generator-webapp 2.1.0
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const browserSync = require('browser-sync');
+const del = require('del');
+const wiredep = require('wiredep').stream;
 
+const browserify = require('browserify');
+const babelify = require('babelify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
 
-//Watch
+const replace = require('gulp-replace');
 
-gulp.task('server', function(next) {
-	var connect = require('connect'),
-	serveStatic = require('serve-static');
-	app = connect();
-	app.use(serveStatic('app', {}));
-	app.listen(process.env.PORT || 3000, next);
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+const SITE_URL = 'mysite.dev';
+
+gulp.task('styles', () => {
+  return gulp.src('app/resources/styles/*.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('app/tmp/styles'))
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('watch', ['server'], function() {
+gulp.task('scripts', () => {
+    const b = browserify({
+      entries: 'app/resources/scripts/main.js',
+      transform: babelify,
+      debug: true
+    });
 
-	livereload({ start: true });
+    return b.bundle()
+      .pipe(source('bundle.js'))
+    .pipe($.plumber())
+    .pipe(buffer())
+    .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('app/tmp/scripts'))
+    .pipe(reload({stream: true}));
+});
 
-	gulp.watch('app/resources/css/**').on('change', function(file) {
-		livereload.changed(file.path);
-	});
+function lint(files, options) {
+  return gulp.src(files)
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.eslint(options))
+    .pipe($.eslint.format())
+    .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+}
 
-	gulp.watch('app/resources/sass/**').on('change', function(file){
-		gulp.run('compass');
-	});
+gulp.task('lint', () => {
+  return lint('app/resources/scripts/**/*.js', {
+    fix: true
+  })
+    .pipe(gulp.dest('app/resources/scripts'));
+});
+gulp.task('lint:test', () => {
+  return lint('test/spec/**/*.js', {
+    fix: true,
+    env: {
+      mocha: true
+    }
+  })
+    .pipe(gulp.dest('test/spec/**/*.js'));
+});
 
-	gulp.watch('app/resources/js/**').on('change', function(file){
-		gulp.run('jshint');
-	});
+gulp.task('html', ['styles', 'scripts'], () => {
+  return gulp.src('app/**/*.html')
+    .pipe($.useref({searchPath: ['app/tmp', 'app', '.']}))
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
+    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+    .pipe(gulp.dest('dist'));
+});
 
-	gulp.watch('app/resources/bower_components/**').on('change', function(file){
-		gulp.run('bower');
-	});
+gulp.task('images', () => {
+  return gulp.src('app/resources/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
+    .pipe(gulp.dest('dist/resources/images'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+    .concat('app/resources/fonts/**/*'))
+    .pipe(gulp.dest('app/tmp/fonts'))
+    .pipe(gulp.dest('dist/resources/fonts'));
+});
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*.*',
+    'app/**/.htaccess',
+    '!app/**/*.html',
+    '!app/tmp/*.*'
+  ], {
+    dot: true
+  })
+  .pipe($.if('*.php', $.replace('../app/templates/', '../craft/templates/')))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('extra-resources', () => {
+  return gulp.src([
+    'app/resources/**/*.*',
+    '!app/resources/bower_components/**/*.*',
+    '!app/resources/fonts/**/*.*',
+    '!app/resources/images/**/*.*',
+    '!app/resources/scripts/**/*.*',
+    '!app/resources/styles/**/*.*',
+  ], {
+    dot: true
+  })
+  .pipe(gulp.dest('dist/resources'));
+});
+
+gulp.task('templates', ['html'], () => {
+  // del.bind(null, ['craft/templates/**/*.*']);
+
+  return gulp.src([
+    'dist/templates/**/*.*'
+  ], {
+    dot: true
+  })
+  .pipe(gulp.dest('craft/templates'));
+});
+
+gulp.task('clean', del.bind(null, ['app/tmp', 'dist', 'craft/templates/*.html']));
+
+gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
+
+  browserSync({
+    notify: false,
+    host: SITE_URL,
+    proxy: SITE_URL,
+    port: 9000
+    // server: {
+    //   baseDir: ['app/tmp', 'app'],
+    //   routes: {
+    //     '/bower_components': 'bower_components'
+    //   }
+    // }
+  });
+
+  gulp.watch([
+    'app/templates/**/*.html',
+    'app/resources/images/**/*',
+    'app/tmp/fonts/**/*'
+  ]).on('change', reload);
+
+  gulp.watch('app/resources/styles/**/*.scss', ['styles']);
+  gulp.watch('app/resources/scripts/**/*.js', ['scripts']);
+  gulp.watch('app/resources/fonts/**/*', ['fonts']);
+  gulp.watch('bower.json', ['wiredep', 'fonts']);
 
 });
 
-gulp.task('compass', function(){
-	gulp.src( ['./app/resources/sass/*.scss'], {base: './app/resources/sass/'} )
-		.pipe(compass({
-				config_file: './config.rb',
-				css: './app/resources/css',
-				sass: './app/resources/sass',
-		})).on('error', function(err) {
-				//console.log(err);
-		})
-		.pipe( prefix("last 3 version") )
-		.pipe( gulp.dest('./app/resources/css')
-	);
+gulp.task('serve:dist', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
 });
 
-gulp.task('jshint', function () {
-	return gulp.src(['./app/resources/js/**/*.js', '!./app/resources/js/vendor/**/*.js'])
-	.pipe(jshint())
-	.pipe(jshint.reporter('jshint-stylish'));
+gulp.task('serve:test', ['scripts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    ui: false,
+    server: {
+      baseDir: 'test',
+      routes: {
+        '/scripts': 'app/tmp/scripts',
+        '/bower_components': 'app/resources/bower_components'
+      }
+    }
+  });
+
+  gulp.watch('app/resources/scripts/**/*.js', ['scripts']);
+  gulp.watch('test/spec/**/*.js').on('change', reload);
+  gulp.watch('test/spec/**/*.js', ['lint:test']);
 });
 
-gulp.task('images', ['move'], function () {
-	return gulp.src('./app/resources/images/**/*')
-	.pipe(cache(imagemin({
-		progressive: true,
-		interlaced: true
-	})))
-	.pipe(gulp.dest('./public/resources/images'));
+// inject bower components
+gulp.task('wiredep', () => {
+  gulp.src('app/resources/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/resources/styles'));
 
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      exclude: ['bootstrap-sass'],
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
 });
 
-gulp.task('fonts', ['move'], function () {
-	return gulp.src('./app/resources/fonts/**/*')
-	.pipe(gulp.dest('./public/resources/styles'));
-
+gulp.task('build', ['lint', 'templates', 'images', 'fonts', 'extras', 'extra-resources'], () => {
+  del.sync(['dist/templates/**']);
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
-gulp.task('json', ['move'], function () {
-	return gulp.src('./app/resources/json/**/*')
-	.pipe(gulp.dest('./public/resources/json'));
-
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
 });
-
-//Build
-
-gulp.task('usemin', ['templates'], function() {
-
-	return gulp.src('./app/templates/_layout.html')
-	.pipe(usemin({
-		// js: [uglify()],
-		// bower: [uglify()]
-	}))
-	.pipe(gulp.dest('./craft/templates/'));
-
-});
-
-// Moving templates, why does usemin fuck this up?
-
-gulp.task('templates', function(){
-
-	return gulp.src(['./app/templates/**'])
-	.pipe(gulp.dest('./craft/templates/'));
-
-});
-
-
-
-
-gulp.task('clean:after', ['images', 'fonts', 'json'], function(cb) {
-	del(['craft/templates/resources/*/**', 'craft/templates/resources/', 'craft/templates/styles/'], cb);
-});
-
-gulp.task('move', ['usemin'], function(){
-	//Delete old resources
-	del(['public/resources/**']);
-	//Move
-	return gulp.src(['craft/templates/resources/**'])
-	.pipe(gulp.dest('public/resources/'));
-});
-
-gulp.task('build', ['clean:after'], function(){
-
-
-});
-
-gulp.task('bower', function () {
-	gulp.src('./app/templates/_layout.html')
-	.pipe(wiredep({
-
-	}))
-	.pipe(gulp.dest('./app/templates/'));
-});
-
-gulp.task('default', ['build']);
